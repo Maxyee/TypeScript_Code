859f54a051a36f20d5cdf215e2dd27e2a0be88c6
Maxyee <eyaminkhan00@gmail.com>
2018-08-12 01:45:15 +0600

    learned how to define variables in typescript

---
+++ greeter.ts
@@ -0,0 +1,23 @@
+
+// typescript function code -------------
+function Greeter (person: string)
+{
+    return 'Hello, ' + person;
+}
+
+var user = 'eyamin';
+
+document.body.innerHTML = Greeter(user);
+
+//----------------------------------------
+
+
+//variable in  declaration typescript -----------------
+var a: number;
+var b : boolean;
+var c: string;
+
+a = 10;
+b = true;
+c = 'Julhas';
+//----------------------------------------
